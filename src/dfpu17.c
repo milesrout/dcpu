db975bfc6a15cdd2135d0ac41538b6822b6f63ea
Miles Rout <miles.rout@gmail.com>
2018-09-05 13:33:39 +1200

    dfpu17: Unpack instructions

--- src/dfpu17.c
+++ src/dfpu17.c
@@ -1,3 +1,4 @@
+#include <setjmp.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,6 +6,7 @@
 
 #include "types.h"
 #include "dcpu.h"
+#include "exception.h"
 #include "utils.h"
 #include "dfpu17.h"
 
@@ -15,12 +17,16 @@ struct device_dfpu17 {
 	u16 error;
 	u16 intrmsg;
 
-	/* pointer/index into the dcpu's ram */
+	/**
+	 * pointer/index into the dcpu's ram. this counts down,
+	 * so we initialise it count-high.
+	 */
 	u16 loadstatus;
 	u16 loadptr;
 	u16 loadcount;
 
 	u8  fcreg[4];
+	u8  pc;
 
 	union {
 		struct {
@@ -88,27 +94,311 @@ enum dfpu17_loadstatus {
 
 enum dfpu17_error {
 	ERROR_NONE,
+	ERROR_FAIL,
 };
 
-void dfpu17_cycle(struct hardware *hardware, u16 *dirty, struct dcpu *dcpu)
+void dfpu17_enqueue_interrupt(struct dcpu *dcpu)
 {
-	(void)hardware;
-	(void)dirty;
+	/* cpu interrupts are not yet implemented */
 	(void)dcpu;
 }
 
 void dfpu17_swap_buffers(struct device *device)
 {
 	u16 temporary[512];
+
 	memcpy(temporary,
 	       dfpu17_get(device, word.data0),
 	       512 * sizeof(u16));
+
 	memcpy(dfpu17_get(device, word.data0),
 	       dfpu17_get(device, word.data1),
 	       512 * sizeof(u16));
+
 	memcpy(dfpu17_get(device, word.data1),
 	       temporary,
 	       512 * sizeof(u16));
+}
+
+void dfpu17_halt(struct device *device, struct dcpu *dcpu, int status, int error)
+{
+	dfpu17_get(device, status) = status;
+	dfpu17_get(device, error) = error;
+
+	if (dfpu17_get(device, mode) == MODE_INT) {
+		dfpu17_swap_buffers(device);
+		dfpu17_enqueue_interrupt(dcpu);
+	}
+}
+
+void dfpu17_execute_short(struct hardware *hw, struct dcpu *dcpu, u8 instruction)
+{
+	(void)hw;
+	(void)dcpu;
+
+	switch ((instruction >> 4) & 0x7) {
+	case 0x0: /* one-operand instructions */
+		switch ((instruction >> 2) & 0x3) {
+		case 0x0: /* zero @a */
+			return;
+		case 0x1: /* inc @a */
+			return;
+		case 0x2: /* dec @a */
+			return;
+		case 0x3: /* jmp @a */
+			return;
+		}
+	case 0x1: /* jc @a,@b */
+		return;
+	case 0x2: /* mov @a,@b */
+		return;
+	case 0x3: /* swap @a,@b */
+		return;
+	case 0x4: /* add %r,%s */
+		return;
+	case 0x5: /* sub %r,%s */
+		return;
+	case 0x6: /* mul %r,%s */
+		return;
+	case 0x7: /* div %r,%s */
+		return;
+	}
+}
+
+void dfpu17_execute(struct hardware *hw, struct dcpu *dcpu)
+{
+	u16 instruction = dfpu17_get(hw->device, text)[dfpu17_get(hw->device, pc)++];
+
+	(void)dcpu;
+
+	if ((instruction & 0x8080) == 0x8080) {
+		/* The MSB is executed first. TODO: explain why */
+		dfpu17_execute_short(hw, dcpu, (instruction >> 8) & 0x7f);
+		dfpu17_execute_short(hw, dcpu, (instruction >> 0) & 0x7f);
+		return;
+	}
+
+	switch ((instruction >> 12) & 0xf) {
+	case 0x0: /* nullary, @,@ and @ ops */
+		switch ((instruction >> 11) & 0x1) {
+		case 0x0:
+			switch (instruction & 0x7ff) {
+			case 0x0: /* noop */
+				return;
+			case 0x1: /* wait */
+				dfpu17_get(hw->device, status) = STATUS_WAITING;
+				return;
+			case 0x2: /* halt */
+				dfpu17_halt(hw->device, dcpu, STATUS_IDLE, ERROR_NONE);
+				return;
+			case 0x3: /* fail */
+				dfpu17_halt(hw->device, dcpu, STATUS_IDLE, ERROR_FAIL);
+				return;
+			default:
+				throw("dfpu17opcode", "out of range");
+			}
+		case 0x1:
+			switch ((instruction >> 8) & 0x7) {
+				case 0x0:
+					switch ((instruction >> 4) & 0xf) {
+					case 0x0: /* jc @a,@b */
+						return;
+					case 0x1: /* set @a,@b */
+						return;
+					case 0x2: /* swap @a,@b */
+						return;
+					default: /* [unassigned] */
+						throw("dfpu17opcode", "out of range");
+					}
+				case 0x1:
+					switch ((instruction >> 2) & 0x3f) {
+					case 0x0: /* zero @a */
+						return;
+					case 0x1: /* jmp @a */
+						return;
+					case 0x2: /* inc @a */
+						return;
+					case 0x3: /* dec @a */
+						return;
+					default: /* [unassigned] */
+						throw("dfpu17opcode", "out of range");
+					}
+				case 0x2:
+					switch ((instruction >> 6) & 0x3) {
+					case 0x0: /* ld %a,@b */
+						return;
+					case 0x1: /* st @b,%a */
+						return;
+					case 0x2: /* rnd @b,%a */
+						return;
+					case 0x3: /* [unassigned] */
+						throw("dfpu17opcode", "out of range");
+					}
+				case 0x7: /* jmp $b */
+					return;
+				default: /* [unassigned] */
+					throw("dfpu17opcode", "out of range");
+			}
+		}
+	case 0x1: /* various @/$ ops */
+		switch ((instruction >> 10) & 0x3) {
+		case 0x0: /* loop @a,$b */
+			return;
+		case 0x1: /* jc @a,$b */
+			return;
+		case 0x2: /* set @a,$b */
+			return;
+		case 0x3: /* [unassigned] */
+			return;
+		}
+	case 0x2: /* ld %,$ */
+		return;
+	case 0x3: /* st %,$ */
+		return;
+	case 0x4: /* %,% and % ops */
+		switch ((instruction >> 11) & 0x1) {
+		case 0x0:
+			switch ((instruction >> 8) & 0x7) {
+			case 0x0:
+				switch ((instruction >> 4) & 0xf) {
+				case 0x0: /* sin %a */
+					return;
+				case 0x1: /* cos %a */
+					return;
+				case 0x2: /* tan %a */
+					return;
+				case 0x3: /* asin %a */
+					return;
+				case 0x4: /* acos %a */
+					return;
+				case 0x5: /* atan %a */
+					return;
+				case 0x6: /* sqrt %a */
+					return;
+				case 0x7: /* rnd %a */
+					return;
+				case 0x8: /* log %a */
+					return;
+				case 0x9: /* log2 %a */
+					return;
+				case 0xa: /* ln %a */
+					return;
+				case 0xb: /* [unassigned] */
+					throw("dfpu17opcode", "out of range");
+				case 0xc: /* abs %a */
+					return;
+				default:  /* [unassigned] */
+					throw("dfpu17opcode", "out of range");
+				}
+			case 0x1:
+				switch ((instruction >> 4) & 0xf) {
+				case 0x0: /* ldz %a */
+					return;
+				case 0x1: /* ld1 %a */
+					return;
+				case 0x2: /* ldpi %a */
+					return;
+				case 0x3: /* lde %a */
+					return;
+				case 0x4: /* ldsr2 %a */
+					return;
+				case 0x5: /* ldphi %a */
+					return;
+				case 0x6: /* [unassigned] */
+					return;
+				case 0x7: /* [unassigned] */
+					return;
+				case 0x8: /* ldl2e %a */
+					return;
+				case 0x9: /* ldl2t %a */
+					return;
+				case 0xa: /* ldlg2 %a */
+					return;
+				case 0xb: /* ldln2 */
+					throw("dfpu17opcode", "out of range");
+				default:  /* [unassigned] */
+					throw("dfpu17opcode", "out of range");
+				}
+			default: /* [unassigned] */
+				throw("dfpu17opcode", "out of range");
+			}
+		case 0x1:
+			switch ((instruction >> 8) & 0x7) {
+			case 0x0: /* mov %a,%b */
+				return;
+			case 0x1: /* xchg %a,%b */
+				return;
+			case 0x2: /* add %a,%b */
+				return;
+			case 0x3: /* mul %a,%b */
+				return;
+			case 0x4: /* sub %a,%b */
+				return;
+			case 0x5: /* rsub %a,%b */
+				return;
+			case 0x6: /* div %a,%b */
+				return;
+			case 0x7: /* rdiv %a,%b */
+				return;
+			}
+		}
+	case 0x5: /* comparisons */
+		switch ((instruction >> 10) & 0x3) {
+		case 0x0: /* lt @c,%a,%b */
+			return;
+		case 0x1: /* gt @c,%a,%b */
+			return;
+		case 0x2: /* eq @c,%a,%b */
+			return;
+		case 0x3: /* ne @c,%a,%b */
+			return;
+		}
+	case 0x6: /* atan %a,%y,%x */
+		return;
+	case 0x7: /* fma  %a,%b,%c*/
+		return;
+	}
+
+	/* all possible cases are covered above */
+	abort();
+}
+
+void dfpu17_cycle(struct hardware *hw, u16 *dirty, struct dcpu *dcpu)
+{
+#define COUNT dfpu17_get(hw->device, loadcount)
+#define PTR dfpu17_get(hw->device, loadptr)
+#define TEXT dfpu17_get(hw->device, text)
+#define DATA dfpu17_get(hw->device, word.data1)
+
+	/* the DFPU-17 is not memory-mapped and thus doesn't care about dirty */
+	(void)dirty;
+
+	if (dfpu17_get(hw->device, mode) == MODE_OFF)
+		return;
+
+	if (dfpu17_get(hw->device, status) == STATUS_RUNNING)
+		dfpu17_execute(hw, dcpu);
+
+	if (COUNT != 0) {
+		switch (dfpu17_get(hw->device, loadstatus)) {
+		case LOADSTATUS_NONE:
+			break;
+		case LOADSTATUS_LOADING_TEXT:
+			TEXT[--COUNT] = dcpu->ram[--PTR];
+			break;
+		case LOADSTATUS_LOADING_DATA:
+			DATA[--COUNT] = dcpu->ram[--PTR];
+			break;
+		case LOADSTATUS_STORING_DATA:
+			dcpu->ram[--PTR] = DATA[--COUNT];
+			break;
+		}
+	}
+
+#undef DATA
+#undef TEXT
+#undef PTR
+#undef COUNT
 }
 
 void dfpu17_interrupt(struct hardware *hw, struct dcpu *dcpu)
@@ -127,17 +417,17 @@ void dfpu17_interrupt(struct hardware *hw, struct dcpu *dcpu)
 		dcpu->registers[3] = 0;
 		break;
 	case LOAD_DATA:
-		dfpu17_get(hw->device, loadptr) = dcpu->registers[0];
+		dfpu17_get(hw->device, loadptr) = dcpu->registers[0] + 512;
 		dfpu17_get(hw->device, loadcount) = 512;
 		dfpu17_get(hw->device, loadstatus) = LOADSTATUS_LOADING_DATA;
 		break;
 	case LOAD_TEXT:
-		dfpu17_get(hw->device, loadptr) = dcpu->registers[0];
+		dfpu17_get(hw->device, loadptr) = dcpu->registers[0] + 256;
 		dfpu17_get(hw->device, loadcount) = 256;
 		dfpu17_get(hw->device, loadstatus) = LOADSTATUS_LOADING_TEXT;
 		break;
 	case GET_DATA:
-		dfpu17_get(hw->device, loadptr) = dcpu->registers[0];
+		dfpu17_get(hw->device, loadptr) = dcpu->registers[0] + 512;
 		dfpu17_get(hw->device, loadcount) = 512;
 		dfpu17_get(hw->device, loadstatus) = LOADSTATUS_STORING_DATA;
 		break;
